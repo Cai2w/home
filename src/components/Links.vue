be4170c4a4b5ef923d3af83b25c0879dbce4c825
wangpx <wangpx@mail.tsinghua.edu.cn>
2024-12-27 15:22:16 +0800

    refactor(components): 更新图标并添加新链接
    - 在 Links.vue 中添加 ChartBar 图标
    - 更新 siteLinks.json 中的站点监测链接
    - 修改链接名称为"监测"

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { FortAwesome, Tags, Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { ChartBar, FortAwesome, Tags, Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -73,6 +73,7 @@ const siteIcon = {
   Link,
   Tags,
   FortAwesome,
+  ChartBar,
 };
 
 // 链接跳转
